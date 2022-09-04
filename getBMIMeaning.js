14e6238cc2571be4f37651c7871b1aafb0996921
ChaySittichok <sittichok.3345@mail.kmutt.ac.th>
2022-09-04 20:37:03 +0700

    update

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,19 @@
-const { template } = require('@babel/core')
+// const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  let BMI = weight / (height * height)
+	return BMI;
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  let BMI = calculateBMI(weight, height)
+     if (BMI < 18.5) {
+        return 'Underweight';
+       } else if (BMI <= 24.9) {
+         return 'Normal weight';
+       } else {BMI > 25.0
+         return 'Overweight';
+       }
 }
-module.exports = getBMIMeaning
+// module.exports = getBMIMeaning
+
+console.log(getBMIMeaning(44, 1.6))
